21a3e1521f30aa8168ab462d2838c7ff5edbedc4
Venus Ang <venusang24@gmail.com>
2018-11-05 17:15:00 -0800

    adding repo

---
+++ src/components/Carousel.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="carousel" >
+    <carousel :pullDrag="true" :autoplay="true" :responsive="{0:{items:1,nav:false},600:{items:3,nav:true}}">
+      <div class="thumbnail" v-for="photo in photos">
+        <img class="thumbnail__image" v-bind:src="require(`../assets/${photo.imgUrl}`)" :alt="`${photo.alt}`"/>
+        <div class="thumbnail__description">{{ photo.description }}</div>
+      </div>
+    </carousel>
+  </div>
+</template>
+
+<script>
+import carousel from 'vue-owl-carousel';
+import json from '../api/photos.json';
+
+export default {
+  components: { carousel },
+  name: 'Carousel',
+  data() {
+    return {
+      photos: json.photos,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.thumbnail {
+  &__description {
+    width: 50%;
+    text-align: left;
+  }
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+
+import Router from 'vue-router';
+import Carousel from '@/components/Carousel';
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'Carousel',
+      component: Carousel,
+    },
+  ],
+});
